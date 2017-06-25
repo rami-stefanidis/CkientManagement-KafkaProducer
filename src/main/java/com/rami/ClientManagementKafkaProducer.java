5075f48ce65dc375210b4a2df18d530b36c7d8e3
Rami <rami.stefanidis@gmail.com>
2017-06-25 17:43:03 -0400

    Communicate with Eureka

--- src/main/java/com/rami/ClientManagementKafkaProducer.java
+++ src/main/java/com/rami/ClientManagementKafkaProducer.java
@@ -2,17 +2,18 @@ package com.rami;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by Rami Stefanidis  on 6/25/2017.
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClientManagementKafkaProducer
 {
     public static void main( String[] args )
     {
-
         SpringApplication.run(ClientManagementKafkaProducer.class, args);
     }
 }
