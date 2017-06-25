f5461df07c24c148fdb716a0ef1ff00c451eafed
Rami <rami.stefanidis@gmail.com>
2017-06-25 15:42:03 -0400

    Initial Project Setup

---
+++ src/main/java/com/rami/ClientManagementKafkaProducer.java
@@ -0,0 +1,18 @@
+package com.rami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Rami Stefanidis  on 6/25/2017.
+ *
+ */
+@SpringBootApplication
+public class ClientManagementKafkaProducer
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(ClientManagementKafkaProducer.class, args);
+    }
+}
